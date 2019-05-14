d2f707166e4b69d3271258e91c16d71315c8d477
Rıza Gökay Kıvırcıoğlu <riza.gokay@gmail.com>
2019-05-14 11:35:04 +0300

    Added readme and modified files

---
+++ README.md
@@ -0,0 +1,2 @@
+### Mrp Run Scheduler Automation
+An application designed for triggering run_scheduler automatically after sale order confirmation or mrp production creation.
--- models/models.py
+++ models/models.py
@@ -4,13 +4,15 @@ import threading
 import logging
 
 from odoo import api, models, tools
+
 _logger = logging.getLogger(__name__)
+
 
 class StockSchedulerCompute(models.TransientModel):
     _inherit = "stock.scheduler.compute"
 
     def _procure_calculation_orderpoint_x(self):
-        #1
+        # 1
         with api.Environment.manage():
             # As this function is in a new thread, I need to open a new cursor, because the old one may be closed
             new_cr = self.pool.cursor()
@@ -31,29 +33,7 @@ class StockSchedulerCompute(models.TransientModel):
                     use_new_cursor=self._cr.dbname,
                     company_id=company.id)
             new_cr.close()
-        #2
-        with api.Environment.manage():
-            # As this function is in a new thread, I need to open a new cursor, because the old one may be closed
-            new_cr = self.pool.cursor()
-            self = self.with_env(self.env(cr=new_cr))
-            scheduler_cron = self.sudo().env.ref('stock.ir_cron_scheduler_action')
-            # Avoid to run the scheduler multiple times in the same time
-            try:
-                with tools.mute_logger('odoo.sql_db'):
-                    self._cr.execute("SELECT id FROM ir_cron WHERE id = %s FOR UPDATE NOWAIT", (scheduler_cron.id,))
-            except Exception:
-                _logger.info('Attempt to run procurement scheduler aborted, as already running')
-                self._cr.rollback()
-                self._cr.close()
-                return {}
-
-            for company in self.env.user.company_ids:
-                self.env['procurement.group'].run_scheduler(
-                    use_new_cursor=self._cr.dbname,
-                    company_id=company.id)
-            new_cr.close()
-
-        #3
+        # 2
         with api.Environment.manage():
             # As this function is in a new thread, I need to open a new cursor, because the old one may be closed
             new_cr = self.pool.cursor()
@@ -75,7 +55,7 @@ class StockSchedulerCompute(models.TransientModel):
                     company_id=company.id)
             new_cr.close()
 
-        #4
+        # 3
         with api.Environment.manage():
             # As this function is in a new thread, I need to open a new cursor, because the old one may be closed
             new_cr = self.pool.cursor()
@@ -97,7 +77,7 @@ class StockSchedulerCompute(models.TransientModel):
                     company_id=company.id)
             new_cr.close()
 
-        #5
+        # 4
         with api.Environment.manage():
             # As this function is in a new thread, I need to open a new cursor, because the old one may be closed
             new_cr = self.pool.cursor()
@@ -119,7 +99,7 @@ class StockSchedulerCompute(models.TransientModel):
                     company_id=company.id)
             new_cr.close()
 
-        #6
+        # 5
         with api.Environment.manage():
             # As this function is in a new thread, I need to open a new cursor, because the old one may be closed
             new_cr = self.pool.cursor()
@@ -141,7 +121,7 @@ class StockSchedulerCompute(models.TransientModel):
                     company_id=company.id)
             new_cr.close()
 
-        #7
+        # 6
         with api.Environment.manage():
             # As this function is in a new thread, I need to open a new cursor, because the old one may be closed
             new_cr = self.pool.cursor()
@@ -163,7 +143,29 @@ class StockSchedulerCompute(models.TransientModel):
                     company_id=company.id)
             new_cr.close()
 
-        #8
+        # 7
+        with api.Environment.manage():
+            # As this function is in a new thread, I need to open a new cursor, because the old one may be closed
+            new_cr = self.pool.cursor()
+            self = self.with_env(self.env(cr=new_cr))
+            scheduler_cron = self.sudo().env.ref('stock.ir_cron_scheduler_action')
+            # Avoid to run the scheduler multiple times in the same time
+            try:
+                with tools.mute_logger('odoo.sql_db'):
+                    self._cr.execute("SELECT id FROM ir_cron WHERE id = %s FOR UPDATE NOWAIT", (scheduler_cron.id,))
+            except Exception:
+                _logger.info('Attempt to run procurement scheduler aborted, as already running')
+                self._cr.rollback()
+                self._cr.close()
+                return {}
+
+            for company in self.env.user.company_ids:
+                self.env['procurement.group'].run_scheduler(
+                    use_new_cursor=self._cr.dbname,
+                    company_id=company.id)
+            new_cr.close()
+
+        # 8
         with api.Environment.manage():
             # As this function is in a new thread, I need to open a new cursor, because the old one may be closed
             new_cr = self.pool.cursor()
@@ -185,7 +187,7 @@ class StockSchedulerCompute(models.TransientModel):
                     use_new_cursor=self._cr.dbname,
                     company_id=company.id)
             new_cr.close()
-        #9
+        # 9
         with api.Environment.manage():
             # As this function is in a new thread, I need to open a new cursor, because the old one may be closed
             new_cr = self.pool.cursor()
@@ -206,7 +208,7 @@ class StockSchedulerCompute(models.TransientModel):
                     use_new_cursor=self._cr.dbname,
                     company_id=company.id)
             new_cr.close()
-        #10
+        # 10
         with api.Environment.manage():
             # As this function is in a new thread, I need to open a new cursor, because the old one may be closed
             new_cr = self.pool.cursor()
@@ -234,3 +236,27 @@ class StockSchedulerCompute(models.TransientModel):
         threaded_calculation = threading.Thread(target=self._procure_calculation_orderpoint_x, args=())
         threaded_calculation.start()
         return {'type': 'ir.actions.act_window_close'}
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.multi
+    def action_confirm(self):
+        sale_order = super(SaleOrder, self).action_confirm()
+        # Hack for running schedulers
+        stock_scheduler = self.env['stock.scheduler.compute']
+        stock_scheduler.procure_calculation()
+        return sale_order
+
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    @api.model
+    def create(self, values):
+        production = super(MrpProduction, self).create(values)
+        # Hack for running schedulers
+        stock_scheduler = self.env['stock.scheduler.compute']
+        stock_scheduler.procure_calculation()
+        return production
